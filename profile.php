33ce20ab2c9d620d6e690005793edc5b84e5be9a
Vidhan Chandra <vidhansharma01@users.noreply.github.com>
2018-02-21 22:51:56 +0530

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Profile</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="profile.js"></script>
+  <link rel="stylesheet" type="text/css" href="profile.css">
+  <link rel="stylesheet" type="text/css" href="bootstrap_new.css">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+      
+    <!-- handberger icon -->
+    <div class="navbar-header">
+    	<a class="navbar-brand" href="#">Infinxx</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only"></span>
+
+             <span class="navbar-toggle-icon">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+             </span>
+            </button>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+         <form class="navbar-form navbar-left " role="search">
+         <div class="form-group">
+             <input type="text" class="form-control" placeholder="Search">
+         </div>
+         <!--
+             <button type="submit" class="btn btn-default">
+             <span class="glyphicon glyphicon-search"></span>
+             </button>
+
+         -->
+         </form>
+
+         <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Profile</a></li>
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-arrow-up"></span> Upload</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-option-horizontal"></span></a></li>
+        </ul>
+  </div>
+</div>
+</nav>
+
+</body>
+</html>
